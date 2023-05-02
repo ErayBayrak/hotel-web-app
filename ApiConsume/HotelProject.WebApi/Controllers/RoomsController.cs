7696ce06d9dc838d1941a1a61b453367ae1c067d
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-05-02 13:44:09 +0300

    Room and Staff Controllers created in Web api.

---
+++ ApiConsume/HotelProject.WebApi/Controllers/RoomsController.cs
@@ -0,0 +1,41 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddRoom(Room room)
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteRoom(Room room)
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom(Room room)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetRoom(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffsController.cs
@@ -0,0 +1,41 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddStaff(Staff staff)
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStaff(Staff staff)
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStaff(Staff staff)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStaff(int id)
+        {
+            return Ok();
+        }
+    }
+}
