351d013f2a514e603b9dc9e6397ea4681bcb6e46
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-05-02 12:20:01 +0300

    Add project files.

---
+++ HotelProject.CoreLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HotelProject.CoreLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ HotelProject.EntityLayer/Concrete/HotelService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class HotelService
+    {
+        public int HotelServiceId { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
