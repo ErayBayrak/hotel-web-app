3b748776332292739c63b1834e1112649340ab3d
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-05-07 14:29:48 +0300

    IContactService

---
+++ HotelProject.BusinessLayer/Abstract/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Abstract
+{
+   public interface IContactService
+    {
+        void Add(Contact contact);
+        void Delete(Contact contact);
+        void Update(Contact contact);
+        Contact Get(Expression<Func<Contact, bool>> filter);
+        List<Contact> GetAll(Expression<Func<Contact, bool>> filter = null);
+    }
+}
---
+++ HotelProject.BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,39 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        public void Add(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact Get(Expression<Func<Contact, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> GetAll(Expression<Func<Contact, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
